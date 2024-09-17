a4541a8ed8fe6a25bfffee058eef2661522f3eda
gaberagland <annie0sc@mail.com>
2024-09-17 10:25:52 -0700

    Add cohere LLM (#7)
    * initial
    
    * bug fixes and prompt improvments
    
    * added Cohere LLM
    improvements and bug fixes
    
    * added observability with ARIZE
    
    * added initial observability
    
    * bump llama_index version

--- genesis_agentic/__init__.py
+++ genesis_agentic/__init__.py
@@ -3,7 +3,7 @@ genesis_agentic package.
 """
 
 # Define the package version
-__version__ = "0.1.7"
+__version__ = "0.1.8"
 
 # Import classes and functions from modules
 # from .module1 import Class1, function1
@@ -12,10 +12,11 @@ __version__ = "0.1.7"
 
 # Any initialization code
 def initialize_package():
-    print("Initializing genesis-agentic package...")
+    print(f"Initializing genesis-agentic version {__version__}...")
 
 
 initialize_package()
+
 
 # Define the __all__ variable
 # __all__ = ['Class1', 'function1', 'Class2', 'function2']
