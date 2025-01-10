51c81fa657579475d663b9881e460f19ec231e79
gaberagland <annie0sc@mail.com>
2025-01-09 19:40:46 -0800

    fixed lint issues (#48)

--- genesis_agentic/__init__.py
+++ genesis_agentic/__init__.py
@@ -2,7 +2,8 @@
 genesis_agentic package.
 """
 
-from ._version import __version__
+from .agent import Agent
+from .tools import GenesisToolFactory, GenesisTool
 
 # Define the __all__ variable for wildcard imports
 __all__ = ['Agent', 'GenesisToolFactory', 'GenesisTool']
